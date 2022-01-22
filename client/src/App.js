65c3f3fa13d146e8818a8619d37e1cbb2bd3d0ea
MohsinFSD <mohsinshahzadfsd@gmail.com>
2022-01-22 14:24:04 +0500

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,7 @@
-import { useState } from "react";
-import Modal from "./ui/Modal/Modal";
-
 function App() {
-  const [isModal, setIsModal] = useState(false)
-  const onDelete = (id, closeHandler) => {
-    closeHandler();
-
-  }
   return (
-    <div style={{ height: '200vh' }}>
-      <button onClick={() => setIsModal(true)}>Open Modal</button>
-
-
-      <Modal
-        type="alert"
-        title='Are you sure?'
-        component={openHandler => <button onClick={openHandler}>Independent Modal</button>}
-        footer={closeHandler => (
-          <>
-            <button className="tx-danger" onClick={() => onDelete('product_id', closeHandler)}>Yes</button>
-            <button className="tx-grey" onClick={closeHandler}>No</button>
-          </>
-        )}
-        description='Are you really want to delete this item?'
-      />
+    <div >
+      
 
     </div>
   )
--- client/src/hoc/Portal.jsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-
-const Portal = ({ children, id = 'modal-wrapper' }) => ReactDOM.createPortal(children, document.getElementById(id));
-
-export default Portal;
