3fc4bb14893284dc6425dc31f50b13ae6a9b563c
Ubaid Badar <m.ubaidbadar@gmail.com>
2022-01-22 10:51:29 +0500

    design buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div></div>
+  return (
+    <div className='d-flex'>
+      <button className='btn-primary'>Loading Button <span className='loading'></span></button>
+      <button className='btn-icon no-padding'><span className="material-icons">shopping_cart</span></button>
+      <button className='btn-border tx-primary'>Button Border</button>
+    </div>
+  )
 }
 
 export default App;
