3f22ec04fbeb9ee6119f4dc65651c2f07b7b9720
Ubaid Badar <m.ubaidbadar@gmail.com>
2022-01-29 16:36:34 +0500

    updated

--- client/src/ui/AlertModal/AlertModal.jsx
+++ client/src/ui/AlertModal/AlertModal.jsx
@@ -5,7 +5,7 @@ const AlertModal = ({title, description, onClose, footer, Footer, ...props}) => 
     <>
         <h3 className="m-0">{title}</h3>
         <p className="tx-grey">{description}</p>
-        <div className={`${styles.footer} gap-3 justify-content-end d-flex align-items-end`}>
+        <div className={`${styles.footer} btn-blank-parent btn-hover-parent btn-normal-parent gap-3 justify-content-end d-flex align-items-end`}>
             {Footer && <Footer onClose={onClose} />}
             {footer && footer(onClose)}
         </div>
