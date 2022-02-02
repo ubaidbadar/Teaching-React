1172d7db0ba9253c1e832f77eb94d9f9aadc5124
Ubaid Badar <m.ubaidbadar@gmail.com>
2022-02-02 11:33:21 +0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,7 @@
-import Modal from "./ui/Modal/Modal";
-
 function App() {
   return (
-    <div style={{ height: '200vh' }}>
-      <Modal component={openHandler => (
-        <button className="btn-primary" onClick={openHandler}>Open Modal</button>
-      )}>
-        <input style={{width: '200%'}} />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-        <input className="w-100" />
-      </Modal>
+    <div>
+      
 
     </div>
   )
